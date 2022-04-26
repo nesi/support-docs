c6bf37b2fad7699dbf8e437050d9f78c2cfbfeb0
Cal <callumnmw@gmail.com>
2022-04-27 10:47:19 +1200

    add linter to build checks

--- custom_hooks.py
+++ custom_hooks.py
@@ -1,6 +1,21 @@
 import linkcheckmd as lc
+import proselint as pl
+import glob
+from pathlib import Path
+import json
 
 
-def test(*args, **kwargs):
+def check_links(*args, **kwargs):
     print("running link checker")
-    lc.check_links("docs", ext=".md", recurse=True, use_async=True)
+    # lc.check_links("docs", ext=".md", recurse=True, use_async=True)
+
+
+def lint(*args, **kwargs):
+    output = {}
+    print("running linter")
+    for file in glob.iglob("docs/**/*.md", recursive=True):
+        with open(file, "r") as f:
+            output[Path(file).stem] = pl.tools.lint(f.read())
+    with open("lint_report.json", "w+") as f:
+        f.write(json.dumps(output))
+    print(output)
