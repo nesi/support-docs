654d7d14a0434a5a6bb99aef8bbd2e5532186ac6
cal <callum.walley@nesi.org.nz>
2023-11-06 12:34:20 +1300

    Ignore admonations

--- checks/run_proselint.py
+++ checks/run_proselint.py
@@ -6,7 +6,14 @@ from proselint import config
 
 files = sys.argv[1:]
 
-for file in files:
+ret_code = 0
+proselint.config.default["checks"]["hyperbole.misc"] = False
+
+for file in files: 
     with open(file, "r", encoding="utf8") as f:
         for notice in proselint.tools.lint(f.read(), config=config.default):
+            if (notice[7] == "error"):
+                ret_code = 1
             print(f"::{notice[7]} file={file},line={notice[2]},col={notice[3]},endLine={notice[2]+notice[6]},title={notice[0]}::'{notice[1]}'")
+
+sys.exit(ret_code)
