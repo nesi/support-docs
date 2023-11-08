22ab924cafc6101cfc9d7f99d6811218e741b3c3
cal <callum.walley@nesi.org.nz>
2023-11-08 22:55:58 +1300

    fix parse markdownlint

--- checks/parse_markdownlint.py
+++ checks/parse_markdownlint.py
@@ -9,6 +9,7 @@ import sys
 
 j = json.load(sys.stdin)
 for m in j:
+    error_range = ""
     if "errorRange" in m and m["errorRange"]:
         error_range = f"col={m['errorRange'][0]},endcol={m['errorRange'][1]},"
     print(f"::warning file={m['fileName']},line={m['lineNumber']},{error_range}title={m['ruleDescription']}::{m['errorDetail']}")
