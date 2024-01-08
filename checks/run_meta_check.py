97d11647a94cd6a4b4f75f55c378bb374746c680
cal <callum.walley@nesi.org.nz>
2024-01-09 11:07:28 +1300

    Added more snippets

--- checks/run_meta_check.py
+++ checks/run_meta_check.py
@@ -19,22 +19,22 @@ MIN_TAGS = 2
 
 # Warning level for missing parameters.
 EXPECTED_PARAMETERS = {
-    "title",
-    "template",
-    "description",
-    "icon",
-    "status",
-    "prereq",
-    "postreq",
-    "suggested",    # Add info here when implimented.
-    "created_at",
-    "hidden",
-    "tags",         # Add info here when implimented.
-    "weight",
-    "vote_count",
-    "vote_sum",
-    "zendesk_article_id",
-    "zendesk_section_id",
+    "title": "",
+    "template": ["main.html", "supported_apps.html"],
+    "description": "",
+    "icon": "",
+    "status": ["new", "deprecated"],
+    "prereq": "",
+    "postreq": "",
+    "suggested": "",    # Add info here when implimented.
+    "created_at": "",
+    "hidden": [True, False],
+    "tags": "",         # Add info here when implimented.
+    "weight": "",
+    "vote_count": "",
+    "vote_sum": "",
+    "zendesk_article_id": "",
+    "zendesk_section_id": "",
 }
 
 
@@ -60,17 +60,15 @@ def main():
             title_from_h1 = _title_from_h1()
             title = meta["title"] if "title" in meta else "" or title_from_h1 or title_from_filename
 
-            for check in ERROR:
-                _run_check(check, "error")
-            for check in WARNING:
-                _run_check(check, "warning")
-            for check in NOTICE:
-                _run_check(check, "notice")
+            for check in CHECKS:
+                _run_check(check)
 
 
-def _run_check(f, level):
+def _run_check(f):
     for r in f():
-        message = r.pop('message')
+        message = r.pop('message', 'something wrong')
+        level = r.pop('level', 'warning')  # Defult is warning.
+
         print(f"::{level} file={input_file},title={f.__name__},{','.join(f'{k}={v}' for k,v in r.items())}::{message}")
 
 
@@ -100,42 +98,60 @@ def _get_lineno(pattern):
 def title_redundant():
     lineno = _get_lineno(r"^title:.*$")
     if "title" in meta.keys() and title_from_filename == meta["title"]:
-        yield {"line": lineno, "message": "Title set in meta is redundant as it is already set in filename."}
+        yield {"level": "notice", "line": lineno, "message": "Title set in meta is redundant as it is already set in filename."}
     if "title" in meta.keys() and title_from_h1 == meta["title"]:
-        yield {"line": lineno, "message": "Title set in h1 is redundant as it is already set in filename."}
+        yield {"level": "notice", "line": lineno, "message": "Title set in h1 is redundant as it is already set in filename."}
     if title_from_filename == title_from_h1:
-        yield {"line": lineno, "message": "Title set in meta is redundant as it is already set in h1."}
+        yield {"level": "notice", "line": lineno, "message": "Title set in meta is redundant as it is already set in h1."}
 
 
 def meta_unexpected_key():
     """
     Check for unexpected keys.
     """
-    for key in meta.keys():
-        if key not in EXPECTED_PARAMETERS:
-            yield {"line": _get_lineno(r"^" + key + r":.*$"), "message": f"Unexpected parameter in front-matter '{key}'"}
+    for key, value in meta.items():
+        if key not in EXPECTED_PARAMETERS.keys():
+            yield {"line": _get_lineno(r"^" + key + r":.*$"),
+                   "message": f"Unexpected parameter in front-matter '{key}'"}
+        elif EXPECTED_PARAMETERS[key] and value not in EXPECTED_PARAMETERS[key]:
+            yield {"level": "error", "line": _get_lineno(r"^status:.*$"),
+                   "message": f"'{value}' is not valid for {key}. [{','.join(EXPECTED_PARAMETERS[key])}]"}
 
 
 def meta_missing_description():
     if "description" not in meta.keys() or len(meta["description"]) < 1:
-        yield {"line":0, "message": "Missing 'description' from front matter."}
+        yield {"line": 0, "message": "Missing 'description' from front matter."}
 
 
 def title_length():
     if len(title) > MAX_TITLE_LENGTH:
-        yield {"line": _get_lineno(r"^title:.*$"), "message": f"Title '{title}' is too long."}
+        yield {"line": _get_lineno(r"^title:.*$"),
+               "message": f"Title '{title}' is too long. \
+Try to keep it under {MAX_TITLE_LENGTH} characters to avoid word wrapping in the nav."}
 
 
 def minimum_tags():
     if "tags" not in meta or not isinstance(meta["tags"], list):
         yield {"line": 0, "message": "'tags' property in meta is missing or malformed."}
     elif len(meta["tags"]) < MIN_TAGS:
-        yield {"line": _get_lineno(r"^tags:.*$"), "message": "Try to include at least 2 'tags' (helps with search optimisation)."}
+        yield {"line": _get_lineno(r"^tags:.*$"), "message": "Try to include at least 2 'tags'\
+(helps with search optimisation)."}
 
 
-ERROR = []
-WARNING = [title_length, meta_missing_description, meta_unexpected_key, minimum_tags]
-NOTICE = [title_redundant]
+def click_here():
+    """
+    not currenlty identified by vscode for some reaon.
+    """
+    i = 0
+    for line in contents.split("\n"):
+        i += 1
+        m = re.search(r"\[here\]\(.*\)", line)
+        if m:
+            yield {"line": i, "col": m.start()+1, "endColumn": m.end()-1, "message":
+                   "Don't use 'here' for link text, impedes accessability."}
+
+
+CHECKS = [title_redundant, title_length, meta_missing_description, meta_unexpected_key, minimum_tags, click_here]
 
 
 if __name__ == "__main__":
