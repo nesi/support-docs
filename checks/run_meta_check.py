6769c09dfd1dd95fb30c8e9169f75c044995ffc6
cal <callum.walley@nesi.org.nz>
2024-01-16 15:55:35 +1300

    Fix tasks I hope.

--- checks/run_meta_check.py
+++ checks/run_meta_check.py
@@ -40,7 +40,7 @@ EXPECTED_PARAMETERS = {
 
 def main():
     input_files = sys.argv[1:]
-    global title, title_from_h1, title_from_filename, input_file, meta, contents, title
+    global title, title_from_h1, title_from_filename, input_file, meta, contents, title, line, lineno
 
     for input_file in input_files:
         if any(re.match(pattern, input_file) for pattern in EXCLUDED_FROM_CHECKS):
@@ -59,17 +59,18 @@ def main():
             title_from_filename = _title_from_filename()
             title_from_h1 = _title_from_h1()
             title = meta["title"] if "title" in meta else "" or title_from_h1 or title_from_filename
-
-            for check in CHECKS:
+            lineno = 0
+            for line in contents.split("\n"):
+                lineno += 1
+                for check in WALKCHECKS:
+                    _run_check(check)
+            for check in ENDCHECKS:
                 _run_check(check)
 
 
 def _run_check(f):
     for r in f():
-        message = r.pop('message', 'something wrong')
-        level = r.pop('level', 'warning')  # Defult is warning.
-
-        print(f"::{level} file={input_file},title={f.__name__},{','.join(f'{k}={v}' for k,v in r.items())}::{message}")
+        print(f"::{r.get('level', 'warning')} file={input_file},title={f.__name__},col={r.get('col', 0)},endColumn={r.get('endColumn', 99)},line={r.get('line', 1)}::{r.get('message', 'something wrong')}")
 
 
 def _title_from_filename():
@@ -120,7 +121,7 @@ def meta_unexpected_key():
 
 def meta_missing_description():
     if "description" not in meta.keys() or len(meta["description"]) < 1:
-        yield {"line": 0, "message": "Missing 'description' from front matter."}
+        yield {"message": "Missing 'description' from front matter."}
 
 
 def title_length():
@@ -132,7 +133,7 @@ Try to keep it under {MAX_TITLE_LENGTH} characters to avoid word wrapping in the
 
 def minimum_tags():
     if "tags" not in meta or not isinstance(meta["tags"], list):
-        yield {"line": 0, "message": "'tags' property in meta is missing or malformed."}
+        yield {"message": "'tags' property in meta is missing or malformed."}
     elif len(meta["tags"]) < MIN_TAGS:
         yield {"line": _get_lineno(r"^tags:.*$"), "message": "Try to include at least 2 'tags'\
 (helps with search optimisation)."}
@@ -140,18 +141,62 @@ def minimum_tags():
 
 def click_here():
     """
-    not currenlty identified by vscode for some reaon.
+    Click [here](for more details)
     """
-    i = 0
-    for line in contents.split("\n"):
-        i += 1
-        m = re.search(r"\[here\]\(.*\)", line)
-        if m:
-            yield {"line": i, "col": m.start()+1, "endColumn": m.end()-1, "message":
-                   "Don't use 'here' for link text, impedes accessability."}
+    m1 = re.search(r"\[.*\s?here\s?.*\]\(.*\)", line, re.IGNORECASE)
+    if m1:
+        yield {"line": lineno, "col": m1.start()+1, "endColumn": m1.end()-1, "message": "Don't use 'here' for link text, impedes accessability."}
+
+    # Impliment check for html links when I can be fd.
+    # m2 = re.search(r"\[here\]\(.*\)", line)
 
 
-CHECKS = [title_redundant, title_length, meta_missing_description, meta_unexpected_key, minimum_tags, click_here]
+def fix_headers():
+    """Pretty poor attempt, but better than nothing"""
+    last_header_level = 1
+    bump_header = False
+    in_code_block = False
+
+    lines = x.splitlines()
+
+    def parse_line(line):
+        nonlocal last_header_level
+        nonlocal bump_header
+        nonlocal in_code_block
+        # If match start of code block
+        if re.match(r"^```", line):
+            in_code_block = not in_code_block
+        if in_code_block:
+            return line
+        header_match = re.match(r"^(#+)\s*(.*)$", line)
+        if not header_match:
+            return line
+        if not header_match.group(2):
+            log.info("Deleting empty header. '%s'", line)
+            return ""
+        header_level = len(header_match.group(1))
+        # If H1, turn on bump
+        if header_level < 2:
+            bump_header = True
+        if bump_header:
+            log.info("Bumping H%s to H%s. '%s'", header_level, header_level+1, line)
+            header_level += 1
+
+        # If more than 1 step down. Change by 1
+        while last_header_level < header_level-1:
+            log.info("Debumping H%s to H%s. '%s'", header_level, header_level-1, line)
+            header_level -= 1
+
+        last_header_level = header_level
+        return re.sub(r"^(#+)", header_level * "#", line)
+
+    return "\n".join([parse_line(line) for line in lines])
+
+# For checks to run on page as a whole
+ENDCHECKS = [title_redundant, title_length, meta_missing_description, meta_unexpected_key, minimum_tags, click_here]
+
+# Checks to be run on each line
+WALKCHECKS = [title_redundant, title_length, meta_missing_description, meta_unexpected_key, minimum_tags, click_here]
 
 
 if __name__ == "__main__":
