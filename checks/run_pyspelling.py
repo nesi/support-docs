5ff8a7134c9a3d949132dab4332a2e4782b699ae
Maxime Rio <maxime.rio@nesi.org.nz>
2024-02-14 02:17:30 +0000

    reformat spelling script

--- checks/run_pyspelling.py
+++ checks/run_pyspelling.py
@@ -16,19 +16,32 @@ if __name__ == "__main__":
         with open(source) as f:
             print(f"Running Pyspelling on {f}")
             source_md = f.readlines()
-        results = spellcheck(".spellcheck.yml", names=["Markdown"], sources=[source], verbose=0, debug=True)
+
+        results = spellcheck(
+            ".spellcheck.yml",
+            names=["Markdown"],
+            sources=[source],
+            verbose=0,
+            debug=True,
+        )
+
         for r in results:
             if not r.words:
                 continue
+
             for word in r.words:
                 line_no = 0
                 for line in source_md:
                     line_no += 1
                     for word in r.words:
-                        matches = re.finditer(r"[^a-zA-Z`._/[\\-]+(" + word + r")[^a-zA-Z`_/\\-]+", line)
+                        matches = re.finditer(
+                            r"[^a-zA-Z`._/[\\-]+(" + word + r")[^a-zA-Z`_/\\-]+", line
+                        )
                         for m in matches:
-                            print(f"::warning file={source},line={line_no},col={m.start()+3},endColumn={m.end()},\
-title=spelling::Word '{word}' is mispeled.")
+                            print(
+                                f"::warning file={source},line={line_no},col={m.start()+3},endColumn={m.end()},\
+title=spelling::Word '{word}' is mispeled."
+                            )
                             sys.stdout.flush()
 
         sys.exit(0)
