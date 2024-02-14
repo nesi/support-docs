de014d509e38723a34bb372e1c7753501f331cd4
Maxime Rio <maxime.rio@nesi.org.nz>
2024-02-15 09:41:30 +1300

    Fix tasks reporting (#141)
    * reformat spelling script
    
    * remove unused variable, use flush option from print(), fix spelling error
    
    * change tone of comment about pyspelling ;-)
    
    * simplify pyspelling postprocessing using flashtext
    
    The flashtext is used to directly find the misspelled words in the
    original document, with their column span.
    
    * fix/simplify pattern matcher for spellchecker tasks
    
    - severity doesn't need to be specified as it is deduced from message
    - "pattern" doesn't need to be a list
    - loop is not needed as each output line is parsed independently
    - endColumn need to be specified instead of endLine
    
    * remove unused module
    
    * add a hack to allow run_pyspelling messages to be parsed by vscode
    
    It looks like VSCode (at least in github codespace) only parses the first
    error reported by run_pyspelling.py. Adding a delay at the end seems to
    fix this issue, as it might prevent the early exit of whatever is parsing
    the command output in VSCode.
    
    See the following issue on github for an idea of the problem:
    https://github.com/microsoft/vscode/issues/92868
    
    * simplify tasks definition
    
    - no need for loop in problemMatcher
    - no need for a list of regex in problemMatcher
    - no need for default severity as it is parsed from messages
    
    * use the ugly hack to fix message parsing in vscode on run_meta_check.py
    
    * refactor run_proselint.py (remove dead code and reformat)
    
    * load content file using pathlib in run_proselint.py
    
    As the file is fully loaded anyway, this avoids keeping the file open
    more time than necessary.

--- checks/run_proselint.py
+++ checks/run_proselint.py
@@ -5,24 +5,27 @@ Modify proselint outputs into a format recognised by github actions.
 """
 
 import sys
+from pathlib import Path
+
 import proselint
 from proselint import config, tools
 
 if __name__ == "__main__":
 
-    ret_code = 0
-
     files = sys.argv[1:]
 
     # Load defaults from config.
-    config_custom = tools.load_options(config_file_path=".proselint.json", conf_default=config.default)
+    config_custom = tools.load_options(
+        config_file_path=".proselint.json", conf_default=config.default
+    )
 
     for file in files:
         print(f"Running proselint on {file}")
-        with open(file, "r", encoding="utf8") as f:
-            for notice in proselint.tools.lint(f.read(), config=config_custom):
-                ret_code += 1
-                print(f"::{notice[7]} file={file},line={notice[2]+1},col={notice[3]+2},endColumn={notice[2]+notice[6]+1},title={notice[0]}::'{notice[1]}'", flush=True)
-                sys.stdout.flush()
-    sys.exit(0)
-    # sys.exit(ret_code)
+        content = Path(file).read_text(encoding="utf8")
+        for notice in proselint.tools.lint(content, config=config_custom):
+            print(
+                f"::{notice[7]} file={file},line={notice[2]+1},"
+                f"col={notice[3]+2},endColumn={notice[2]+notice[6]+1},"
+                f"title={notice[0]}::'{notice[1]}'",
+                flush=True,
+            )
