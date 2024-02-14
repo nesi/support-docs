12ede1a8851153065af97a05f55f775fa518d727
Maxime Rio <maxime.rio@nesi.org.nz>
2024-02-14 10:55:03 +0000

    refactor run_proselint.py (remove dead code and reformat)

--- checks/run_proselint.py
+++ checks/run_proselint.py
@@ -10,19 +10,20 @@ from proselint import config, tools
 
 if __name__ == "__main__":
 
-    ret_code = 0
-
     files = sys.argv[1:]
 
     # Load defaults from config.
-    config_custom = tools.load_options(config_file_path=".proselint.json", conf_default=config.default)
+    config_custom = tools.load_options(
+        config_file_path=".proselint.json", conf_default=config.default
+    )
 
     for file in files:
         print(f"Running proselint on {file}")
         with open(file, "r", encoding="utf8") as f:
             for notice in proselint.tools.lint(f.read(), config=config_custom):
-                ret_code += 1
-                print(f"::{notice[7]} file={file},line={notice[2]+1},col={notice[3]+2},endColumn={notice[2]+notice[6]+1},title={notice[0]}::'{notice[1]}'", flush=True)
-                sys.stdout.flush()
-    sys.exit(0)
-    # sys.exit(ret_code)
+                print(
+                    f"::{notice[7]} file={file},line={notice[2]+1},"
+                    f"col={notice[3]+2},endColumn={notice[2]+notice[6]+1},"
+                    f"title={notice[0]}::'{notice[1]}'",
+                    flush=True,
+                )
