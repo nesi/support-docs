49c9b58532ca73d9bbb2697050d20387f3d2a84b
cal <callum.walley@nesi.org.nz>
2024-08-21 15:33:06 +1200

    update template to use new module list format

--- docs/assets/javascripts/general.js
+++ docs/assets/javascripts/general.js
@@ -1,8 +1,8 @@
-function changeVersion(app, machine, version, ){
+function changeVersion(app, version, comment = ""){
     // Sets the module load example to use the selected version
-    document.getElementById("mod_" + app + "_" + machine).innerHTML=`module load ${app}/${version}`
-    document.querySelectorAll(".md-tags-ver-" + app + "-" + machine + ">.md-tag-ver-shown").forEach((e) => e.classList.remove("md-tag-ver-shown"))
-    document.getElementById("mod_" + app + "_" + machine + "_" + version).classList.add("md-tag-ver-shown")
+    document.getElementById("mod_" + app.toLowerCase()).innerHTML=`module load ${app}/${version}${comment ? "\n # " + (comment) : ""}`
+    document.querySelectorAll(".md-tags-ver-" + app.toLowerCase() +">.md-tag-ver-shown").forEach((e) => e.classList.remove("md-tag-ver-shown"))
+    document.getElementById("mod_" + app.toLowerCase() + "_" + version).classList.add("md-tag-ver-shown")
     // ew. so gross
 }
 function toggle(id){
