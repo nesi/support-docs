fdd85cf98144aa4c758111c2980bc0bcc66c10e4
cal <callum.walley@nesi.org.nz>
2024-08-23 22:41:42 +1200

    FIxed escaping. i think. Also aadded proper warning

--- docs/assets/javascripts/general.js
+++ docs/assets/javascripts/general.js
@@ -1,8 +1,15 @@
-function changeVersion(app, version, comment = ""){
+function changeVersion(app, version, warn = false){
     // Sets the module load example to use the selected version
-    document.getElementById("mod_" + app.toLowerCase()).innerHTML=`module load ${app}/${version}${comment ? "\n # " + (comment) : ""}`
+    document.getElementById("mod_" + app.toLowerCase() + "_code").innerHTML=`module load ${app}/${version}`
     document.querySelectorAll(".md-tags-ver-" + app.toLowerCase() +">.md-tag-ver-shown").forEach((e) => e.classList.remove("md-tag-ver-shown"))
     document.getElementById("mod_" + app.toLowerCase() + "_" + version).classList.add("md-tag-ver-shown")
+    if (warn){
+        document.getElementById("mod_" + app.toLowerCase() + "_warn").style.display = "block";
+        document.getElementById("mod_" + app.toLowerCase() + "_warn").querySelector("p.warning-text").innerHTML = document.getElementById("mod_" + app.toLowerCase() + "_" + version).title;
+    }else{
+        document.getElementById("mod_" + app.toLowerCase() + "_warn").style.display = "none";
+    }
+
     // ew. so gross
 }
 function toggle(id){
