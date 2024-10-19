5f474f5d2fa20324e763aac0a3984b9fea11009a
cal <callum.walley@nesi.org.nz>
2024-10-19 20:28:53 +1300

    fix missing link param

--- docs/assets/javascripts/updateFeed.js
+++ docs/assets/javascripts/updateFeed.js
@@ -38,6 +38,7 @@ async function update() {
                 allChannel.push({
                     date: new Date(pubDateText),
                     channel: channelName,
+                    link: item.querySelector("link")?.textContent || "",
                     channelUrl: feed,
                     title: item.querySelector("title")?.textContent || "", 
                     description: item.querySelector("description")?.textContent || "", 
@@ -55,18 +56,25 @@ async function update() {
     // Collect all innerHTML content before updating the DOM to minimize reflows
     allChannel.forEach(f => {
         try{  
+
+            title = `<h3>${f.title}</h3>`;
+            if (f.link){
+                title = `<a class="md-feed-title" href="${f.link}">${title}</a>`
+            }
             htmlContent += `
-                <div class="feed-item">
-                    <h3 href="${f.link}">${f.title}</h3>
-                    <div><p>${f.description}</p></div>
+                <div class="md-feed-item">
+                    ${title}
+                    <div class="md-feed-description"><p>${f.description}</p></div>
                     <div>
                     <span class="md-icon md-feed-date" title="Date">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21 13.1c-.1 0-.3.1-.4.2l-1 1 2.1 2.1 1-1c.2-.2.2-.6 0-.8l-1.3-1.3c-.1-.1-.2-.2-.4-.2m-1.9 1.8-6.1 6V23h2.1l6.1-6.1-2.1-2M12.5 7v5.2l4 2.4-1 1L11 13V7h1.5M11 21.9c-5.1-.5-9-4.8-9-9.9C2 6.5 6.5 2 12 2c5.3 0 9.6 4.1 10 9.3-.3-.1-.6-.2-1-.2s-.7.1-1 .2C19.6 7.2 16.2 4 12 4c-4.4 0-8 3.6-8 8 0 4.1 3.1 7.5 7.1 7.9l-.1.2v1.8Z"></path></svg>
-                    <span>${f.date.toLocaleDateString()} via <a target="_self"  href="${f.channelUrl}">${f.channel}</a></span>
+                    <span>${f.date.toLocaleDateString()} via <a target="_blank" href="${f.channelUrl}">${f.channel}</a></span>
                     </span>
                     </span>
                     </div>
                 </div>`;
+            console.log(f);
+
         }catch (error) {
                     console.error("Error fetching or parsing feeds: ", error);
     }
