ad1c116d1eba484355d476456dacc293dc9f5f9a
cal <callum.walley@nesi.org.nz>
2024-12-02 10:56:42 +1300

    Adds some simple checks to slurm code blocks.

--- checks/run_slurm_lint.py
+++ checks/run_slurm_lint.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-Runs checks on article meta block and outputs in github action readable format
+Runs checks on slurm scrips found in code and outputs in github action readable format
 """
 
 __author__ = "cal w"
@@ -15,11 +15,13 @@ from pathlib import Path
 LINES_AFTER_SHEBANG = 1
 LINES_AFTER_HEADER = 1
 SHEBANG = r"^#!\/bin\/bash -e\s*$"
-SBATCH_HEADER = r"^#SBATCH\s*(-[\S]*)([\s=]*)(\S*.*?)(#.*)?$"
+SBATCH_HEADER = r"^(.*)#SBATCH\s*(-[^=\s]*)([\s=]*)(\S*.*?)(#.*)?$"
 SBATCH_DELIM = r"=|\s+"
 
 REQUIRED_SBATCH_HEADER = [
-    {"long": "--job-name", "short": "-j"}
+    {"long": "--job-name", "short": "-j"},
+    {"long": "--account", "short": "-a"},
+    {"long": "--time", "short": "-t"},
 ]
 
 
@@ -48,16 +50,25 @@ def main():
 
 def parse_script(start_linno, indent, slurm):
     global step, n_lines_after_shebang, n_lines_after_header, line, uses_equals_delim, uses_whitespace_delim
-    global match_header_line
+    global match_header_line, start_of_header, slurm_headers
+
+    uses_equals_delim = False
+    uses_whitespace_delim = False
 
     def _run_check(f):
         for r in f():
-            print(f"::{r.get('level', 'warning')} file={input_path},title={f.__name__},col={r.get('col', 0) + indent},endColumn={r.get('endColumn', 99) + indent},line={start_linno + lineno}::{r.get('message', 'something wrong')}")
+            print(f"::{r.get('level', 'warning')} file={input_path},title={f.__name__}," +
+                  f"col={r.get('col', 0) + indent},endColumn={r.get('endColumn', 99) + indent}," +
+                  f"line={start_linno + r.get('line', lineno)}::{r.get('message', 'something wrong')}")
             sys.stdout.flush()
             time.sleep(0.01)
 
     n_lines_after_shebang = 0
     n_lines_after_header = 0
+    slurm_headers = []
+
+    start_of_header = 0
+    end_of_header = 0
     step = 0    # 0 : shebang
                 # 1 : slurm header
                 # 2 : bash
@@ -72,6 +83,7 @@ def parse_script(start_linno, indent, slurm):
             elif re.match(SBATCH_HEADER, line):
                 _run_check(lines_after_shebang)
                 step = 1
+                start_of_header = lineno
             else:
                 _run_check(content_before_slurm_header)
         if step == 1:
@@ -79,11 +91,15 @@ def parse_script(start_linno, indent, slurm):
                 n_lines_after_header += 1
                 continue
             match_header_line = re.match(SBATCH_HEADER, line)
-
+            slurm_headers.append(match_header_line)
             if match_header_line:
-                for check in SBATCH_HEADER_CHECKS:
+                for check in SBATCH_HEADER_WALK:
                     _run_check(check)
-        #     else:
+            else:
+                step = 2
+                end_of_header = line
+                for check in SBATCH_HEADER_ALL:
+                    _run_check(check)
 
         # elif :
 
@@ -91,7 +107,7 @@ def parse_script(start_linno, indent, slurm):
 def finditer2(pattern, string, flags):
     """
     A version of ``re.finditer`` that returns ``(match, line_number)`` pairs.
-    """ 
+    """
     matches = list(re.finditer(pattern, string, flags))
     if matches:
         end = matches[-1].start()
@@ -115,16 +131,18 @@ def slurm_shebang():
     if not re.match(SHEBANG, line):
         yield {"message": f"Your shebang was '{line}', should use '{SHEBANG}'"}
 
+
 def lines_after_shebang():
     if n_lines_after_shebang != LINES_AFTER_SHEBANG:
         yield {"level": "notice", "message": f"There are {n_lines_after_shebang} blank lines after the shebang, should be {LINES_AFTER_SHEBANG}."}
+
 
 def content_before_slurm_header():
     yield {"level": "error", "message": f"There is text ('{line}') between the shebang and slurm header. This is not a valid SLURM script."}
 
 
 def malformed_delimiter():
-    delim = match_header_line.group(2)
+    delim = match_header_line.group(3)
     if delim == "=":
         uses_equals_delim = True
     elif delim.isspace():
@@ -133,14 +151,32 @@ def malformed_delimiter():
         yield {"level": "error", "message": f"'{delim}' is not a valid SLURM header delimiter."}
 
 
+def inconsistant_delimiter():
+    if uses_equals_delim and uses_whitespace_delim:
+        yield {"message": "Header uses both whitespace and '=' delimiters.", "line": start_of_header}
+
+
 def short_option():
-    if not match_header_line.group(1)[:2] == "--":
-        yield {"level": "info", "col": 8, "endColumn": 8 + len(match_header_line.group(1)), "message": f"Using short form flag '{match_header_line.group(1)}'. Long form is prefered."}
+    if not match_header_line.group(2)[:2] == "--":
+        yield {"level": "info", "col": 8, "endColumn": 8 + len(match_header_line.group(2)), 
+               "message": f"Using short form flag '{match_header_line.group(2)}'. Long form is prefered."}
 
 
+def minimum_options():
+    for header in REQUIRED_SBATCH_HEADER:
+        for h in slurm_headers:
+            if not h:
+                continue
+            a = h.group(2)
+            if a == header["long"] or a == header["short"]:
+                break
+        else:
+            yield {"message": f"Script header must contain \'{header['long']}\'.",
+                   "line": start_of_header}
 
-SBATCH_HEADER_CHECKS = [malformed_delimiter, short_option]
 
+SBATCH_HEADER_WALK = [malformed_delimiter, short_option]
+SBATCH_HEADER_ALL = [inconsistant_delimiter, minimum_options]
 
 if __name__ == "__main__":
     main()
