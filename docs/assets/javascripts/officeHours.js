ad9565f70da1c1fb66f9a5562e1e33063fc4c408
Cal <35017184+CallumWalley@users.noreply.github.com>
2026-04-22 13:59:05 +1200

    Update officeHours.js
    Signed-off-by: Cal <35017184+CallumWalley@users.noreply.github.com>

--- docs/assets/javascripts/officeHours.js
+++ docs/assets/javascripts/officeHours.js
@@ -1,5 +1,4 @@
 async function displayOfficeHoursCalendar() {
-    console.log("script added");
     const calendarDiv = document.getElementById('office-hours-calendar');
     const calendarP = document.getElementById('office-hours-calendar-message');
     const calendarButtons = document.getElementById('office-hours-buttons');
@@ -58,7 +57,6 @@ async function displayOfficeHoursCalendar() {
         calendarDiv.appendChild(tableWrap);
         calendarP.innerHTML = '';
         if (upcomingEvents.length > 0) {
-            console.log(upcomingEvents[0]);
             calendarButtons.innerHTML=`<a href="webcal://assets/training_calendar.ics" download class="md-button md-button--addcal"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M680-80v-120H560v-80h120v-120h80v120h120v80H760v120h-80Zm-480-80q-33 0-56.5-23.5T120-240v-480q0-33 23.5-56.5T200-800h40v-80h80v80h240v-80h80v80h40q33 0 56.5 23.5T760-720v244q-20-3-40-3t-40 3v-84H200v320h280q0 20 3 40t11 40H200Zm0-480h480v-80H200v80Zm0 0v-80 80Z"/></svg>Add to Calendar</a><a href="${upcomingEvents[0].meetingLink}" class="md-button md-button--addcal"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h480q33 0 56.5 23.5T720-720v180l160-160v440L720-420v180q0 33-23.5 56.5T640-160H160Zm0-80h480v-480H160v480Zm0 0v-480 480Z"/></svg>Join Zoom Meeting</a>`;
         } else {
             calendarButtons.innerHTML = 'No Upcoming Office Hours';
